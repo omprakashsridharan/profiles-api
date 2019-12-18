cda3ff89e29ce3ceb9303403cd240faf35cb7a83
omprakash <omprakashsridharan@outlook.com>
2019-12-18 21:31:29 +0530

    Adds profile view set with authentication

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
+from profiles_api import serializers, models, permissions
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 
 class HelloApiView(APIView):
@@ -16,7 +18,7 @@ class HelloApiView(APIView):
             'gives you the most control over app logic',
             'mapped manually to URLs'
         ]
-        return Response({'message':'Hello','an_apiview':an_apiview})
+        return Response({'message':'Hello','an_apivieew':an_apiview})
 
     def post(self,request):
         """Create hello message with name"""
@@ -28,3 +30,17 @@ class HelloApiView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+class HelloViewSet(viewsets.ViewSet):
+    """Test API view sets"""
+
+    def list(self,request):
+        """List any data"""
+        sample_list = ["a","b","c"]
+        return Response({'message':'hello','data':sample_list})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
